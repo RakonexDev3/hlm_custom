106e03412eabbaf8ff4da4519f8c5a81e84d4525
Aravind R <aravindrajeshchry@gmail.com>
2026-04-13 18:15:21 +0530

    feat: Add Packing List doctype

---
+++ halamama_custom/halamama_custom/doctype/packing_list/packing_list.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Rakonex and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Packing List", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ halamama_custom/halamama_custom/doctype/packing_list/test_packing_list.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Rakonex and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPackingList(FrappeTestCase):
+	pass
