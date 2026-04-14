7a31da937ef36f42c883e09d7400cc7a99deed35
Aravind R <aravindrajeshchry@gmail.com>
2026-04-14 15:22:05 +0530

    feat(Packing List): Add Packing List Item child doctype

---
+++ halamama_custom/halamama_custom/doctype/packing_list_item/packing_list_item.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Rakonex and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PackingListItem(Document):
+	pass
