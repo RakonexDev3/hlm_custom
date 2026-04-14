73093c4930e65ab92adf1d8696355972f0083662
Aravind R <aravindrajeshchry@gmail.com>
2026-04-14 15:21:33 +0530

    feat(Packing List): Add script to auto-fill supplier/warehouse details

--- halamama_custom/halamama_custom/doctype/packing_list/packing_list.js
+++ halamama_custom/halamama_custom/doctype/packing_list/packing_list.js
@@ -1,8 +1,36 @@
 // Copyright (c) 2026, Rakonex and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Packing List", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Packing List", {
+	from_document(frm) {
+        if(!frm.doc.from_doctype || !frm.doc.from_document) {
+            return;
+        } else {
+            if(frm.doc.from_doctype == "Purchase Order") {
+                frappe.db.get_value("Purchase Order", frm.doc.from_document, ["supplier", "supplier_name"])
+                .then(r=>{
+                    if(r && r.message){
+                        if(r.message.supplier){
+                            frm.set_value("supplier", r.message.supplier)
+                        }
+                        if(r.message.supplier){
+                            frm.set_value("supplier_name", r.message.supplier_name)
+                        }
+                    }
+                });
+            } else if(frm.doc.from_doctype == "Material Request"){
+                frappe.db.get_value("Material Request", frm.doc.from_document, ["set_from_warehouse", "set_warehouse"])
+                .then(r=>{
+                    if(r && r.message){
+                        if(r.message.set_from_warehouse){
+                            frm.set_value("from_warehouse", r.message.set_from_warehouse)
+                        }
+                        if(r.message.supplier){
+                            frm.set_value("to_warehouse", r.message.set_warehouse)
+                        }
+                    }
+                });
+            }
+        }
+	},
+});
