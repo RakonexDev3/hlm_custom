80b8880cd75b96ed1b0f76f53f34a318bd79ed74
Aravind R <aravindrajeshchry@gmail.com>
2026-04-16 14:48:17 +0530

    chore(Packing List): Separate link fields for Purchase Order & Material Request

--- halamama_custom/halamama_custom/doctype/packing_list/packing_list.js
+++ halamama_custom/halamama_custom/doctype/packing_list/packing_list.js
@@ -2,12 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on("Packing List", {
-	from_document(frm) {
-        if(!frm.doc.from_doctype || !frm.doc.from_document) {
+	from_po(frm) {
+        if(!frm.doc.from_doctype || !frm.doc.from_po) {
             return;
         } else {
             if(frm.doc.from_doctype == "Purchase Order") {
-                frappe.db.get_value("Purchase Order", frm.doc.from_document, ["supplier", "supplier_name"])
+                frappe.db.get_value("Purchase Order", frm.doc.from_po, ["supplier", "supplier_name"])
                 .then(r=>{
                     if(r && r.message){
                         if(r.message.supplier){
@@ -18,34 +18,38 @@ frappe.ui.form.on("Packing List", {
                         }
                     }
                 });
-            } else if(frm.doc.from_doctype == "Material Request"){
-                frappe.db.get_value("Material Request", frm.doc.from_document, ["set_from_warehouse", "set_warehouse"])
-                .then(r=>{
-                    if(r && r.message){
-                        if(r.message.set_from_warehouse){
-                            frm.set_value("from_warehouse", r.message.set_from_warehouse)
-                        }
-                        if(r.message.supplier){
-                            frm.set_value("to_warehouse", r.message.set_warehouse)
-                        }
-                    }
-                });
-            }
+            } else return;
         }
 	},
 
+    from_mr(frm){
+        if(frm.doc.from_doctype == "Material Request"){
+            frappe.db.get_value("Material Request", frm.doc.from_mr, ["set_from_warehouse", "set_warehouse"])
+            .then(r=>{
+                if(r && r.message){
+                    if(r.message.set_from_warehouse){
+                        frm.set_value("from_warehouse", r.message.set_from_warehouse)
+                    }
+                    if(r.message.supplier){
+                        frm.set_value("to_warehouse", r.message.set_warehouse)
+                    }
+                }
+            });
+        }
+    },
+
     fetch_items(frm){
-        if(!frm.doc.from_document){
+        if(!frm.doc.from_po && !frm.doc.from_mr){
             frappe.msgprint({
                 title: "Unable to Fetch",
-                message: "From Document field is empty",
+                message: "Reference Missing.",
                 indicator: "blue"
             });
         } else {
             let ref_doctype = frm.doc.from_doctype;
             
             if(ref_doctype == "Purchase Order"){
-                frappe.db.get_doc(ref_doctype, frm.doc.from_document)
+                frappe.db.get_doc(ref_doctype, frm.doc.from_po)
                 .then(po=>{
                     if(po.items){
                         frm.clear_table("items");
